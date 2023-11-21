fa5b6c225fb94efca6bd908e778ec37c5a6ba393
6worry <dudfhr217@naver.com>
2023-11-22 04:34:10 +0900

    hash 4번까지

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-a = input()
-b = int(a)
-print('%X' % b)
+# 문자열의 길이를 반환
+string_example = "Hello, World!"
+length_of_string = len(string_example)
+print("문자열의 길이:", length_of_string)
+
+# 리스트의 길이를 반환
+list_example = ["1", 2, 3, 4, 5]
+length_of_list = len(list_example)
+print("리스트의 길이:", length_of_list)
+
+# 튜플의 길이를 반환
+tuple_example = (10, 20, 30, 40)
+length_of_tuple = len(tuple_example)
+print("튜플의 길이:", length_of_tuple)
